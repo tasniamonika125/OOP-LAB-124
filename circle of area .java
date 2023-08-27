42310ae334238d2606315606bd6571a420bf78ba
tasniamonika125 <142910298+tasniamonika125@users.noreply.github.com>
2023-08-27 22:51:45 +0600

    Create circle of area .java

---
+++ circle of area .java
@@ -0,0 +1,21 @@
+
+package com.mycompany.circleofarea;
+import java.util.Scanner;
+/**
+ *
+ * @author Tasnia Aman Monika
+ */
+public class Circleofarea {
+
+    public static void main(String[] args) {
+        { Scanner input = new Scanner(System. in);
+        int radius;
+        double area;
+        
+System. out. println("Enter the radius:");
+radius =input.nextInt();
+area = (3.1416*radius*radius);
+System. out. println("Area of ​​Circle is = " + area);
+        }
+    }
+}
